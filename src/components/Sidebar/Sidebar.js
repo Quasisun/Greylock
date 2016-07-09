07651e16a825c29ab75a681a6325fab704b02b4a
Hugh Zhang <hughzhang@gmail.com>
2016-07-09 15:37:07 -0700

    sidebar works

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Sidebar.css';
+import Form from '../Form';
+
 
 function Sidebar() {
   return (
-    <div className={s.Sidebar}> HELLO</div>
+    <div className={s.Sidebar}> <Form /> </div>
   );
 }
 
