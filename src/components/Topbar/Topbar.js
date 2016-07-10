4b0e0b67fdaddaa7e91c96927d74bc4b056b1f28
Jesse Zhang <jessezhang@rzwang-macbookair.roam.corp.google.com>
2016-07-09 19:26:51 -0700

    added topbar component

---
+++ src/components/Topbar/Topbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Topbar.css';
+
+import {
+	Navbar,
+	Nav,
+	NavItem
+} from 'react-bootstrap'
+
+function Topbar () {
+	return(
+	  <div>
+		  <Navbar>
+		    <Navbar.Header>
+		      <Navbar.Brand>
+		        <img src="logo.jpg" style="width:304px;height:228px;">
+		      </Navbar.Brand>
+		    </Navbar.Header>
+		    <Nav>
+		      <NavItem eventKey={1} href="#">Link</NavItem>
+		      <NavItem eventKey={2} href="#">Link</NavItem>
+		    </Nav>
+		  </Navbar>
+	  </div>	
+	);
+}
+
+
+export default withStyles(s)(Topbar);
